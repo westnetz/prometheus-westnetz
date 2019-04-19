9f16c16cb7b17367bda8d1abb098ad8cc18fad6b
Christian Franke <nobody@nowhere.ws>
2019-04-19 17:35:07 +0200

    Maintain acme.json outside of the git to keep state around

--- up.sh
+++ up.sh
@@ -5,8 +5,8 @@ set -e
 PATH="/opt/bin:$PATH"
 
 cd "$(dirname "$0")"
-touch acme.json
-chmod 0600 acme.json
+touch ../prometheus-westnetz-acme.json
+chmod 0600 ../prometheus-westnetz-acme.json
 
 export HOSTNAME="$(hostname -f)"
 
